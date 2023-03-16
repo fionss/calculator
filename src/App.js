a4a3a21780c4dfbdd0b547d399f5ffe676a4a6ea
fionss <52118554+fionss@users.noreply.github.com>
2023-03-16 19:54:32 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import { useReducer } from 'react';
-import "./style.css";
+import './style.css';
 
 const ACTIONS = {
   ADD_DIGIT: 'add-digit',
   CHOOSE_OPERATION: 'choose-operation',
   CLEAR: 'clear',
   DELETE_DIGIT: 'delete-digit',
-  EVALUATE: 'EVALUATE'
-}
+  EVALUATE: 'EVALUATE',
+};
 
-function reducer(state, {type, payload} ) {
-  switch(type) {
-    case ACTIONS.ADD_DIGIT: 
-    return {
-      ...state,
-      currentOperand: `${currentOperand || " "}${payload.digit}`
-    }
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand || ' '}${payload.digit}`,
+      };
   }
-
 }
 
 export default function App() {
-  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
-  
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="previous-operand">
+          {previousOperand} {operation}
+        </div>
         <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
